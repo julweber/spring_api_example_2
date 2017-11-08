3aebb467b6bc60b1aac14912930e305e63bdefbc
Julian Weber <julianweberdev@gmail.com>
2017-11-08 11:32:32 +0100

    adds QUEUE_NAME to env configuration

--- src/main/java/training/Configuration.java
+++ src/main/java/training/Configuration.java
@@ -6,10 +6,12 @@ public class Configuration {
 
   // good example for port binding from environment variables -> PaaS standard
   public final Integer PORT = (System.getenv("PORT") != null) ? Integer.parseInt(System.getenv("PORT")) : 8081;
+  public final String QUEUE_NAME = (System.getenv("QUEUE_NAME") != null) ? System.getenv("QUEUE_NAME") : "messages";
 
   public String toString() {
     String str = "";
     str += String.format("PORT: %d \n", PORT);
+    str += String.format("QUEUE_NAME: %s \n", QUEUE_NAME);
     return str;
   }
 }
